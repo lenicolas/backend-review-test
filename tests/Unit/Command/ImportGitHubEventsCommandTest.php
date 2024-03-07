66e8a7fdf812c6e07237f0151b8c9a699520a625
The <le.nicolas@outlook.com>
2024-03-07 10:45:20 +0100

    add unit test command import sucess

--- tests/Unit/Command/ImportGitHubEventsCommandTest.php
+++ tests/Unit/Command/ImportGitHubEventsCommandTest.php
@@ -7,22 +7,36 @@ use App\Service\GitHubEventsImporterServiceInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class ImportGitHubEventsCommandTest extends TestCase
 {
     public function testExecute(): void
     {
-        $application = new Application();
-        $gitHubEventsImporterService = $this->createMock(GitHubEventsImporterServiceInterface::class);
+        $date = '2024-01-01';
+        $hour = '15';
 
-        $application->add(new ImportGitHubEventsCommand($gitHubEventsImporterService));
+        $this->createMock(InputInterface::class)
+             ->method('getArgument')
+             ->willReturnMap([
+                                 ['date', $date],
+                                 ['hour', $hour]
+                             ]);
 
-        $command = $application->find('app:import-github-events');
-        $commandTester = new CommandTester($command);
-        $result = $commandTester->execute();
+        $importer = $this->createMock(GitHubEventsImporterServiceInterface::class)
+                         ->expects($this->once())
+                         ->method('importEvents')
+                         ->with($date, $hour);
+
+        // Create ImportGitHubEventsCommand instance and inject the mock importer
+        $command = new ImportGitHubEventsCommand($importer);
+        $tester = new CommandTester($command);
+        $result = $tester->execute(['date' => $date, 'hour' => $hour]);
+
         $this->assertSame(Command::SUCCESS, $result);
+        $this->assertStringContainsString('Command Import GH events executed successfully.', $tester->getDisplay());
 
     }
 }
