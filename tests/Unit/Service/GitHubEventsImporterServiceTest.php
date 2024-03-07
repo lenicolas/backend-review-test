ed7fc78e5821e6b7d6e87287c8eb19223044b3f4
The <le.nicolas@outlook.com>
2024-03-07 10:07:18 +0100

    init class unit test service GHeventImporterservice

---
+++ tests/Unit/Service/GitHubEventsImporterServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Unit\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class GitHubEventsImporterServiceTest extends TestCase
+{
+    public function testImportEvent(): void
+    {
+    }
+}
