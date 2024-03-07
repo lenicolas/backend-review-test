b01ebe09362aac241f37c01dc026dfde297ffefa
The <le.nicolas@outlook.com>
2024-03-07 13:41:45 +0100

    rename writeEventRepository

--- src/Repository/WriteEventRepository.php
+++ src/Repository/WriteEventRepository.php
@@ -4,7 +4,17 @@ namespace App\Repository;
 
 use App\Dto\EventInput;
 
-interface WriteEventRepository
+class WriteEventRepository implements WriteEventRepositoryInterface
 {
-    public function update(EventInput $authorInput, int $id): void;
+    public function update(EventInput $authorInput, int $id): void
+    {
+        $sql = <<<SQL
+        UPDATE event
+        SET comment = :comment
+        WHERE id = :id
+SQL;
+        $this->
+
+        $this->connection->executeQuery($sql, ['id' => $id, 'comment' => $authorInput->comment]);
+    }
 }
