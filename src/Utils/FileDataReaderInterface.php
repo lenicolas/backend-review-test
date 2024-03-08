f82cabf5facbff7424d2e2eaa98281c7583904b6
The <le.nicolas@outlook.com>
2024-03-08 11:12:20 +0100

    add comment for phpstan

--- src/Utils/FileDataReaderInterface.php
+++ src/Utils/FileDataReaderInterface.php
@@ -5,7 +5,12 @@ namespace App\Utils;
 interface FileDataReaderInterface
 {
     public function getFileData(string $filename): string;
+
+    /** @return resource */
     public function openFile(string $jsonData);
+
+    /** @param resource $handle */
     public function isEndOfFile($handle): bool;
+    /** @param resource $handle */
     public function readLine($handle): string;
 }
