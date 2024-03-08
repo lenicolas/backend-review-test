1a31cf43212dac79f33769b278b6b2ee9c704f57
The <le.nicolas@outlook.com>
2024-03-08 10:53:36 +0100

    add test for invalid value

--- tests/Unit/Service/GitHubEventsImporterServiceTest.php
+++ tests/Unit/Service/GitHubEventsImporterServiceTest.php
@@ -90,4 +90,107 @@ class GitHubEventsImporterServiceTest extends TestCase
 
         $this->gitHubEventsImporterService->importEvents('2022-01-01', '00');
     }
+
+    public function testImportEventsWithInvalidEvent()
+    {
+        $eventJsonData = '{
+                            "test"
+                        }';
+
+        $this->fileDataReader
+            ->method('getFileData')
+            ->willReturn('data');
+
+        $setup = vfsStream::setup();
+        $fileSystem = new vfsStreamFile('test');
+        $fileSystem->withContent($eventJsonData);
+        $setup->addChild($fileSystem);
+        $handle = fopen($fileSystem->url(), "r");
+
+        $this->fileDataReader
+            ->method('openFile')
+            ->willReturn($handle);
+        $this->fileDataReader
+            ->method('isEndOfFile')
+            ->with($handle)
+            ->willReturn(false, true);
+        $this->fileDataReader
+            ->method('readLine')
+            ->with($handle)
+            ->willReturn($eventJsonData);
+
+        $this->readEventRepository
+            ->expects($this->never())
+            ->method('exist')
+            ->with('test')
+            ->willReturn(false);
+
+        $this->entityManager
+            ->expects($this->never())
+            ->method('persist');
+        $this->entityManager->expects($this->never())
+            ->method('flush');
+
+        $this->gitHubEventsImporterService->importEvents('2022-01-01', '00');
+    }
+
+    public function testImportEventsWithExistEvent()
+    {
+        $eventJsonData = '{
+                            "id": "34619792924",
+                            "type": "PushEvent",
+                            "actor": {
+                                "id": 67855588,
+                                "login": "test",
+                                "display_login": "test",
+                                "gravatar_id": "",
+                                "url": "https:\/\/api.github.com\/users\/test",
+                                "avatar_url": "https:\/\/avatars.githubusercontent.com\/u\/67855588?"
+                            },
+                            "repo": {
+                                "id": 739791410,
+                                "name": "test",
+                                "url": "https:\/\/api.github.com\/repos\/test"
+                            },
+                            "payload": {
+                                "key": "test"
+                            },
+                            "public": true,
+                            "created_at": "2024-01-06T15:00:00Z"
+                        }';
+        $this->fileDataReader
+            ->method('getFileData')
+            ->willReturn('data');
+
+        $setup = vfsStream::setup();
+        $fileSystem = new vfsStreamFile('test');
+        $fileSystem->withContent($eventJsonData);
+        $setup->addChild($fileSystem);
+        $handle = fopen($fileSystem->url(), "r");
+
+        $this->fileDataReader
+            ->method('openFile')
+            ->willReturn($handle);
+        $this->fileDataReader
+            ->method('isEndOfFile')
+            ->with($handle)
+            ->willReturn(false, true);
+        $this->fileDataReader
+            ->method('readLine')
+            ->with($handle)
+            ->willReturn($eventJsonData);
+
+        $this->readEventRepository
+            ->method('exist')
+            ->with('34619792924')
+            ->willReturn(true);
+
+        $this->entityManager
+            ->expects($this->never())
+            ->method('persist');
+        $this->entityManager->expects($this->never())
+            ->method('flush');
+
+        $this->gitHubEventsImporterService->importEvents('2022-01-01', '00');
+    }
 }
